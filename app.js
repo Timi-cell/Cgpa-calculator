a86ba6fe172f22a55175c2eb62b27c1a8dc0960e
Timi-cell <adedayotimilehin10@gmail.com>
2022-10-05 23:43:22 -0700

    did some changes to the web app

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-// let user = prompt(
-//   "Hi welcome to my CGPA calculator!, Can I have your name please?"
-// );
-
-// document.getElementById("name").innerHTML = `Welcome, ${user.toUpperCase()}`;
-// let courses = [];
-// let units = [];
-// let grades = [];
-
-// document.getElementById("submit").addEventListener("click", () => {
-//   let courseName = document.querySelector("#course-code").value;
-//   courses.push(courseName);
-//   document.querySelector("#course-code").value = "";
-//   let courseUnits = parseInt(document.querySelector("#course-units").value);
-//   units.push(courseUnits);
-//   document.querySelector("#course-units").value = "";
-//   let gradeScores = document.querySelector("#grade").value.toUpperCase();
-//   grades.push(gradeScores);
-//   document.querySelector("#grade").value = "";
-// });
+let user = prompt(
+  "Hi welcome to my CGPA calculator!, Can I have your name please?"
+);
+if (user !== "" && user !== null) {
+  document.getElementById("name").textContent = `Current User: ${user}`;
+} else {
+  alert(
+    "You did not enter your name!, You can refresh the page to enter your name"
+  );
+  document.getElementById("name").textContent = `Current User: ----`;
+}
 
 let gradePoints = {
   A: 5,
@@ -37,15 +28,22 @@ document.getElementById("submit").addEventListener("click", () => {
   let courseName = document.querySelector("#course-code").value;
   let courseUnits = parseInt(document.querySelector("#course-units").value);
   let gradeScores = document.querySelector("#grade").value.toUpperCase();
+
   if (courseName !== "" && courseUnits !== "" && gradeScores !== "") {
     coursesFirst.push(courseName);
     document.querySelector("#course-code").value = "";
-    unitsFirst.push(courseUnits);
-    document.querySelector("#course-units").value = "";
+    if (courseUnits <= 5 && courseUnits !== 0) {
+      unitsFirst.push(courseUnits);
+      document.querySelector("#course-units").value = "";
+    } else {
+      alert("Course units should be between 1 and 5");
+      document.querySelector("#course-units").value = "";
+    }
+
     gradesFirst.push(gradeScores);
     document.querySelector("#grade").value = "";
   } else {
-    alert("pls complete all the fields");
+    alert("Please complete all the fields in the First Semester");
   }
 });
 
@@ -66,18 +64,17 @@ document.getElementById("calc").addEventListener("click", () => {
     for (let index = 0; index < unitsFirst.length; index++) {
       total.push(unitsFirst[index] * refinedGrades[index]);
     }
-
+    // calculating the total units given for a semester
     const totalUnits = unitsFirst.reduce((cur, acc) => {
       return cur + acc;
     });
-
     const finalAns = total.reduce((cur, acc) => {
       return cur + acc;
     });
     document.getElementById("gpa_block").style.display = "block";
     document.querySelector(".gpa").textContent = finalAns / totalUnits;
   } else {
-    alert("you have no data inputted yet!");
+    alert("You have no data inputted yet for the 1st semester!");
   }
 });
 
@@ -93,12 +90,18 @@ document.getElementById("submit2").addEventListener("click", () => {
   if (courseName !== "" && courseUnits !== "" && gradeScores !== "") {
     coursesSecond.push(courseName);
     document.querySelector("#course-code2").value = "";
-    unitsSecond.push(courseUnits);
-    document.querySelector("#course-units2").value = "";
+    if (courseUnits <= 5 && courseUnits !== 0) {
+      unitsSecond.push(courseUnits);
+      document.querySelector("#course-units2").value = "";
+    } else {
+      alert("Course units should be between 1 and 5");
+      document.querySelector("#course-units2").value = "";
+    }
+
     gradesSecond.push(gradeScores);
     document.querySelector("#grade2").value = "";
   } else {
-    alert("pls complete all the fields");
+    alert("Please complete all the fields in the Second Semester");
   }
 });
 
@@ -113,33 +116,32 @@ document.getElementById("calc2").addEventListener("click", () => {
     for (let index = 0; index < unitsSecond.length; index++) {
       total_second.push(unitsSecond[index] * refinedGrades[index]);
     }
-
     const totalUnits2 = unitsSecond.reduce((cur, acc) => {
       return cur + acc;
     });
-
     const finalAns2 = total_second.reduce((cur, acc) => {
       return cur + acc;
     });
     document.getElementById("gpa_block2").style.display = "block";
     document.querySelector(".gpa2").textContent = finalAns2 / totalUnits2;
   } else {
-    alert("you have no data inputted yet!");
+    alert("You have no data inputted yet for the 2nd semester!");
   }
 });
 
 // CALCULATION OF CPGA
-
 document.getElementById("cgpa").addEventListener("click", () => {
   let firstGpa = parseInt(document.querySelector(".gpa").textContent);
   let secondGpa = parseInt(document.querySelector(".gpa2").textContent);
   if (!isNaN(firstGpa) && !isNaN(secondGpa)) {
     document.querySelector(
       ".cgpa"
-    ).textContent = `Your CGPA for this session is ${
+    ).textContent = `${user}, Your CGPA for this session is ${
       (firstGpa + secondGpa) / 2
     }`;
   } else {
-    alert("You need to calculate your GPA's first");
+    alert(
+      "You need to calculate your GPA's in the 1st and 2nd semester first!"
+    );
   }
 });
